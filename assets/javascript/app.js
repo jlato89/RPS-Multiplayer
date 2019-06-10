82478c57ec1e5dcccf98d556e24479e370081ac6
Josh Latour <jlatour88@gmail.com>
2019-06-09 23:06:32 -0500

    fixed formatting, added op selection code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyDh1fH6RCqWjbKXD0zAXADvVUfAWRKLVV8",
-authDomain: "rps-multiplayer-6980e.firebaseapp.com",
-databaseURL: "https://rps-multiplayer-6980e.firebaseio.com",
-projectId: "rps-multiplayer-6980e",
-storageBucket: "rps-multiplayer-6980e.appspot.com",
-messagingSenderId: "498198746253",
-appId: "1:498198746253:web:969e96613aaf7c45"
+   apiKey: "AIzaSyDh1fH6RCqWjbKXD0zAXADvVUfAWRKLVV8",
+   authDomain: "rps-multiplayer-6980e.firebaseapp.com",
+   databaseURL: "https://rps-multiplayer-6980e.firebaseio.com",
+   projectId: "rps-multiplayer-6980e",
+   storageBucket: "rps-multiplayer-6980e.appspot.com",
+   messagingSenderId: "498198746253",
+   appId: "1:498198746253:web:969e96613aaf7c45"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -32,21 +32,21 @@ var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
 
 // When the client's connection state changes...
-connectedRef.on("value", function(snap) {
+connectedRef.on("value", function (snap) {
 
-  // If they are connected..
-  if (snap.val()) {
+   // If they are connected..
+   if (snap.val()) {
 
-    // Add user to the connections list.
-    var con = connectionsRef.push(true);
+      // Add user to the connections list.
+      var con = connectionsRef.push(true);
 
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
+      // Remove user from the connection list when they disconnect.
+      con.onDisconnect().remove();
+   }
 });
 
 // When first loaded or when the connections list changes...
-connectionsRef.on("value", function(snapshot) {
+connectionsRef.on("value", function (snapshot) {
 
    // The number of online users is the number of children in the connections list.
    dbConnections = snapshot.numChildren();
@@ -63,70 +63,69 @@ connectionsRef.on("value", function(snapshot) {
 // --- End of Persistance code --- //
 
 // Main Process
-$(document).ready(function() {
+$(document).ready(function () {
 
    // Check to see how many players are ready
-   database.ref("playersReady").on("value", function(snapshot) {
-      console.log("DB Players Ready: "+snapshot.val());
+   database.ref("playersReady").on("value", function (snapshot) {
+      console.log("DB Players Ready: " + snapshot.val());
 
       playersReady = snapshot.val(); //* checks how many players are ready.
    });
 
-//  !START BUTTON!  // 
+   //  !START BUTTON!  // 
    // On start of game, assign player number then show game buttons. 
    // If players is above 2, then show alert/error.
-   $("#startBtn").on("click", function() {
+   $("#startBtn").on("click", function () {
 
       if (playersReady < 2) {
          // console.log('IF -> playerNum: ', playerNum);
          playerID = playersReady + 1;
          playersReady++;
-         console.log("You are player "+playerID);
+         console.log("You are player " + playerID);
 
          $("#startBtn").hide();
          $("#game-buttons").css("visibility", "visible");
 
          database.ref("/").update({
             playersReady: playersReady
-         });        
-      } 
-      else {alert("Only 2 players are allowed at a time!")}
+         });
+      }
+      else { alert("Only 2 players are allowed at a time!") }
    })
 
 
-//  !GAME PROCESS!  //   
+   //  !GAME PROCESS!  //   
    // On game button click, send to database and then show to DOM.
-   $(".game-btns").on("click", function() {
+   $(".game-btns").on("click", function () {
       if (playersReady === 2) {
          selection = $(this).attr("alt");
          selectionImg = $(this).attr("src");
-         playerName = "Player "+playerID;
+         playerName = "P" + playerID;
          console.log(selection + " button clicked");
 
          database.ref("/selections").update({
-            [playerName]: selection
+            [playerName]: selectionImg
          });
-      
-      // post selection to user-selection in DOM
+
+         // post selection to user-selection in DOM
          $("#user-selection").attr("src", selectionImg);
       }
-      else {alert("2nd player isn't ready yet")}
+      else { alert("2nd player isn't ready yet") }
    });
 
    // Check to see when both users have a selection
-   database.ref("/selections").on("value", function(snapshot) {
-    var selections = snapshot.numChildren();
-    var p1 = snapshot.child("selections/Player 2").val();
-    console.log('p1: ', p1);
-    if (selections === 2) {
-      console.log("Both users have made their selection");
+   database.ref("/selections").on("value", function (snapshot) {
+      var selections = snapshot.numChildren();
 
-      if (playerID === 1) {
-         // $("#opponent-selection").attr("src", )
+      if (selections === 2) {
+         console.log("Both users have made their selection");
+
+         if (playerID === 1) {
+            $("#opponent-selection").attr('src', snapshot.val().P2);
+         }
+         if (playerID === 2) {
+            $("#opponent-selection").attr('src', snapshot.val().P1);
+         }
       }
-      if (playerID === 2) {
-        //! query player 1's selection from DB, then display it to the DOM
-      }
-    }
    });
 });
